45da73c6ea40f7de78cbcb7beecd68c7eae6a651
Sviatoslav <sviatoslav.pereverziev@gmail.com>
2018-11-13 21:56:10 +0200

    Changed data selection parameters

--- analytics_api.py
+++ analytics_api.py
@@ -37,7 +37,7 @@ def get_report(analytics):
                 {
                     'viewId': VIEW_ID,
                     'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
-                    'metrics': [{'expression': 'ga:sessions'}],
+                    'metrics': [{'expression': 'ga:sessions'},{'expression': 'ga:newUsers'}],
                     'dimensions': [{'name': 'ga:country'}]
                 }]
         }
